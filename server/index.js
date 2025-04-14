d5b828431ed10003d55c63caad0e661f44828ac3
fnlstudio <fnlstudio1@gmail.com>
2025-04-14 18:35:08 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -8,6 +8,10 @@ const io = new Server(server, {
   cors: { origin: "*" }
 });
 
+app.get("/", (req, res) => {
+  res.send("Server is running.");
+});
+
 io.on("connection", (socket) => {
   console.log("New user connected");
 
@@ -20,6 +24,7 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(3000, () => {
-  console.log("Server running on http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
